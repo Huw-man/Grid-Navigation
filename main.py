90ad77e96a04f951ed264850fa2dec99cb41df2e
Huw-Man <hu.newman@berkeley.edu>
2017-10-08 01:45:41 -0700

    can visualize path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tkinter as tk
+import time
 
 class GridCanvas(tk.Canvas):
     '''Grid object hold nodes'''
@@ -10,11 +11,17 @@ class GridCanvas(tk.Canvas):
         self.ngx, self.ngy, self.ndm, self.adjx, self.adjy = 0,0,0,0,0 #space between nodes, diameter of node, adjustment
         self.bind("<Configure>", self.scale)
 
-    def populate(self):
+    def populate(self, update=False):
         '''fill grid with nodes'''
-        for (r, c), n in np.ndenumerate(self.gridAry):
-            self.gridAry[r,c] = Node('%d, %d'% (r, c), 'pink', [self.adjx+ r*self.ngx,          self.adjy+ c*self.ngy,
-                                                                self.adjx+ r*self.ngx +self.ndm, self.adjy+ c*self.ngy+self.ndm])
+        if not update:
+            for (r, c), n in np.ndenumerate(self.gridAry):
+                self.gridAry[r,c] = Node('%d, %d'% (r, c), 'pink', [self.adjx+ r*self.ngx,          self.adjy+ c*self.ngy,
+                                                                    self.adjx+ r*self.ngx +self.ndm, self.adjy+ c*self.ngy+self.ndm])
+        else: #updates so grid should have nodes
+            for (r, c), n in np.ndenumerate(self.gridAry):
+                self.gridAry[r,c] = Node('%d, %d'% (r, c), n.color, [self.adjx+ r*self.ngx,          self.adjy+ c*self.ngy,
+                                                                    self.adjx+ r*self.ngx +self.ndm, self.adjy+ c*self.ngy+self.ndm])
+
 
     def drawNodes(self, update=False):
         '''draws or update drawn nodes'''
@@ -27,6 +34,7 @@ class GridCanvas(tk.Canvas):
             for (r, c), n in np.ndenumerate(self.gridAry):
                 self.coords(n.name, n.pos)
                 self.coords(n.name+'text', n.center)#[(self.adjx+r*self.ngx) + (self.ndm/2), (self.adjy+c*self.ngy) +(self.ndm/2)])
+                self.itemconfigure(n.name, fill=n.color) #changes nodes color
                 #self.itemconfigure(n.name+'text', text=n.name)
 
     def scale(self, event):
@@ -37,7 +45,7 @@ class GridCanvas(tk.Canvas):
         self.adjx, self.adjy = event.width*0.05, event.height*0.05
         #(event.width - (self.gridAry.shape[0]-1)*self.ngx+self.ndm) /2, (event.height - (self.gridAry.shape[1]-1)*self.ngy+self.ndm) /2
         #scaling so buggy
-        self.populate()
+        self.populate(True)#put in nodes with new attributes
         self.drawNodes(True) #updates
         #self.draw_edges(True) #updates
 
@@ -55,19 +63,30 @@ class GridCanvas(tk.Canvas):
     def set_neighbors(self):
         '''puts the neighbors of each node'''
         grid_shape = self.grid_dimensions()
+        #print(grid_shape)
         for (r, c), n in np.ndenumerate(self.gridAry):
-            node_left = self.gridAry[r, c-1] if c-1 > 0 else []
-            node_right = self.gridAry[r,c+1] if c+1 < grid_shape[1] else []
-            node_up = self.gridAry[r-1,c] if r-1 > 0 else []
-            node_down = self.gridAry[r+1,c] if r+1 < grid_shape[0] else []
+            #print(r,c)
+            node_up = self.gridAry[r, c-1] if c-1 >= 0 else []
+            node_down = self.gridAry[r,c+1] if c+1 < grid_shape[1] else []
+            node_left = self.gridAry[r-1,c] if r-1 >= 0 else []
+            node_right = self.gridAry[r+1,c] if r+1 < grid_shape[0] else []
             n.neighbors = [node_left, node_right, node_up, node_down]
-            print(n.neighbors)
+            n.neighbors = [n for n in n.neighbors if n]
+            #print(n.neighbors)
 
+    def change_node(self, node):
+        x,y = node.name_int()
+        self.gridAry[x,y] = node
+        self.populate(True)
+        self.drawNodes(True)
 
     def grid_dimensions(self):
         return self.gridAry.shape #(x,y)
 
-class Node():
+    def get_grid_array(self): #return gridAry
+        return self.gridAry
+
+class Node:
     def __init__(self, name, color, position):
         '''each point on grid is a node
         position is the coordinates of the box of circle
@@ -93,13 +112,63 @@ class Node():
         '''
         return int(self.name[0]), int(self.name[3])
 
+    def __str__(self):
+        return self.name
+
+def breadth_first_search( _grid, start_node, end_node, _visited = []):
+    grid = _grid
+    print(start_node)
+    #time.sleep(.10)
+    start_node.color = "blue" #change color
+    grid.change_node(start_node)
+    #root.after(100, grid.change_node(start_node))
+    visited = _visited
+    visited.append(start_node) #add self to visited
+    if start_node == end_node: #found node
+        print("found")
+        return visited
+    for neighbor in start_node.neighbors:
+        # print(neighbor)
+        # print(visited)
+        # print(neighbor not in visited)
+        if neighbor not in visited:
+            #visited.append(neighbor)
+            return breadth_first_search(grid, neighbor, end_node, visited)
+    # for node in unvisited:
+    #     #print("visited"+str(visited))
+    #     visited.append(node)
+    #     print(node)
+    #     return breadth_first_search(grid, node, end_node)
+
 if __name__ == '__main__':
     root = tk.Tk()
-    gr = GridCanvas( root, 1, 2, width=500, height=500)
+    gr = GridCanvas( root, 3, 3, width=500, height=500)
     gr.populate()
     gr.drawNodes()
     #gr.draw_edges()
     gr.set_neighbors()
+    gry = gr.get_grid_array()
+    start, end = gry[0,0], gry[2,2]
+    #gry[0,0].color = "blue"
+
+    def wrap_breadth_first_search():
+        path = breadth_first_search(gr, start, end)
+        print(str_path(path))
+
+    def str_path(path):
+        return [str(p) for p in path]
+
+    button = tk.Button( root, text="search", command = wrap_breadth_first_search )
+    button.pack()
+
+
+    #gr.change_node(gry[0,0])
+    #print(gr.get_grid_array()[0,0].color)
+    # print(start)
+    # print(end)
+    # print(start == end)
+    #print( breadth_first_search(gr, start, end) )
+
     gr.pack(side="bottom", fill="both", expand=True)
     tk.mainloop()
 # root = tk.Tk()
